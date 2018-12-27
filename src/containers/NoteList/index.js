05476643503386db82c8db36b7f25f3f7ef9fe11
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-27 13:22:34 -0600

    Finalize note filtering functionality in note list

--- src/containers/NoteList/index.js
+++ src/containers/NoteList/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './NoteList.css';
+
 import { changeFilter } from '../../actions/filter-actions';
+import './NoteList.css';
 
 export class NoteList extends Component {
   constructor(props) {
@@ -18,18 +19,33 @@ export class NoteList extends Component {
     const { changeFilter } = this.props; 
 
     await this.setState({ [name]: value }) 
-    changeFilter(filter);
   }
+
+  filterNotes = (filter) => {
+    const filteredNotes = this.props.notes.filter(note => {
+      return note.filter === filter
+    })
+  } 
 
   render() {
     const { notes } = this.props; 
-    const { tag } = this.state; 
+    const { filter } = this.state; 
     let noteCards;
 
-    if (notes) {
+    if (filter && notes) {
+      let filteredNotes = this.filterNotes(filter);
+
+      if (filteredNotes) {
+        noteCards = filteredNotes.map(filteredNote => <p>{filteredNote.text}</p>)
+      } else {
+        noteCards = <p>There are no notes to display</p>
+      }
+
+    } else if (!filter && notes) {
       noteCards = notes.map(note => <p>{note.text}</p>)
+
     } else {
-      noteCards = <p>'There are no notes to display'</p>
+      noteCards = <p>There are no notes to display</p>
     }
 
     return (
@@ -37,14 +53,16 @@ export class NoteList extends Component {
         <h1>Notes</h1>
           <select
               name='filter' 
-              value={tag} 
+              value={filter} 
               onChange={this.handleChange}>
             <option value=''>Choose a Filter</option>
             <option value='personal'>Personal</option>
             <option value='work'>Work</option>
             <option value='hobby'>Hobby</option>
           </select>
+
         { noteCards }
+
       </div>
     ) 
   }
@@ -54,8 +72,4 @@ const mapStateToProps = (state) => ({
   notes: state.notes,
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  changeFilter: (filter) => dispatch(changeFilter(filter)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(NoteList);
+export default connect(mapStateToProps)(NoteList);
